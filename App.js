a4d4411fa35e62cbdfa75c272897e5d96ec19917
prabhat1530 <152500533+prabhat1530@users.noreply.github.com>
2025-01-17 13:39:08 +0530

    pra

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ const { mongodbConnection } = require('./connection');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use('/api/todo', userRouter);
